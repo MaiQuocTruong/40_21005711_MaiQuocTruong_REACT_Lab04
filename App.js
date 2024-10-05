64c55895009d2dbc0be39d6b154e0a523c53179c
maiquoctruong <nguyenthanhtung0900@gmail.com>
2024-10-06 00:24:19 +0700

    done lab4

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Cau2a from './screens/Cau2a';
+import Cau2c from './screens/Cau2c';
+import TikTokOK from './screens/TikTokOK';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const Stack = createStackNavigator();
+
+// export default function App() {
+//   return (
+//     <>
+   
+//      {/* <Cau2a /> 
+//      <Cau2b /> */}
+//      <Cau2c />
+//     {/* <Tiki_Ok /> */}
+//     </>
+//   );
+// }
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Cau2a">
+        <Stack.Screen name="Cau2a" component={Cau2a} options={{ title: 'Login' }} />
+        <Stack.Screen name="TikTokOK" component={TikTokOK} options={{ title: 'TikTok OK' }} />
+        <Stack.Screen name="Cau2c" component={Cau2c} options={{ title: 'Forgot Password' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
